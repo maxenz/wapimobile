5ebe2a0ef36452b41317871b720ba5bc0499522c
Maximiliano Poggio <maximilianopoggio@gmail.com>
2014-07-24 12:19:24 -0300

    first commit

---
+++ wApiMobile/Controllers/ServiciosController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using wApiMobile.Models;
+
+namespace wApiMobile.Controllers
+{
+    public class ServiciosController : ApiController
+    {
+        // GET api/<controller>
+        public List<Servicio> Get()
+        {
+            List<Servicio> lstServ = new List<Servicio>();
+            Servicio serv = new Servicio("126644", "4KL", 1, "M", 21, "Cabildo 5698", "OSDE", "15:23");
+            Servicio serv2 = new Servicio("887644", "L2L", 2, "F", 10, "Santo Tome 5118", "MEDICUS", "11:23");
+            Servicio serv3 = new Servicio("422644", "98G", 3, "M", 44, "Lacroze 1134", "OSDE", "09:15");
+            lstServ.Add(serv);
+            lstServ.Add(serv2);
+            lstServ.Add(serv3);
+            return lstServ;
+           
+        }
+
+        // GET api/<controller>/5
+        public Servicio Get(int id)
+        {
+            return null;
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ wApiMobile/Models/Servicio.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace wApiMobile.Models
+{
+    public class Servicio
+    {
+        public String IdServicio { get; set; }
+        public String NroServicio { get; set; }
+        public int Grado { get; set; }
+        public String Sexo { get; set; }
+        public int Edad { get; set; }
+        public String Domicilio { get; set; }
+        public String Cliente { get; set; }
+        public String Horario { get; set; }
+
+        public Servicio(string id, string nro, int gdo, string sexo, int edad, string dom, string cli, string hor)
+        {
+            IdServicio = id;
+            NroServicio = nro;
+            Grado = gdo;
+            Sexo = sexo;
+            Edad = edad;
+            Domicilio = dom;
+            Cliente = cli;
+            Horario = hor;
+        }
+    }
+}
