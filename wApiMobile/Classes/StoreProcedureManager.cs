cf8fe718dcee0cdf08fd6ac3a2166f3df6aaf8ac
maxenz <maximilianopoggio@gmail.com>
2014-08-14 14:47:53 -0300

    clase storeProcedureManager primera version

---
+++ wApiMobile/Classes/StoreProcedureManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace wApiMobile.Classes
+{
+    public class StoreProcedureManager
+    {
+        private SqlConnection SqlConn;
+        private SqlCommand SqlCommand;
+        private string ConnStringName;
+        private string ConnString;
+
+        public StoreProcedureManager(SqlConnection sqlConn, string connStringName)
+        {
+            ConnStringName = connS
+        }
+
+    }
+}
